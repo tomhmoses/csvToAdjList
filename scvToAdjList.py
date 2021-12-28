88c2ea243090a319b3fe9f7519a6ca32447103e6
Tom Moses <tom@tmoses.co.uk>
2021-12-28 19:37:42 +0000

    scv sample

---
+++ scvToAdjList.py
@@ -0,0 +1,38 @@
+import csv
+
+
+def convert():
+    adjList = {}
+    names = []
+
+    with open('sample1.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            # if first line
+            if line_count == 0:
+                print(f'Column names are {", ".join(row)}')
+                print(row)
+                # save names for all places
+                names = row
+                line_count += 1
+            
+            # if a line of distances
+            else:
+                print(row)
+                adjList[names[line_count]] = {}
+                for pos in range(len(row)):
+                    if pos != 0 and row[pos]:
+                        startPlace = names[line_count]
+                        destinationPlace = names[pos]
+                        distance = int(row[pos])
+                        print(f'Distance from {startPlace} to {destinationPlace} is {distance}')
+                        adjList[names[line_count]][destinationPlace] = distance
+
+
+                line_count += 1
+        print(f'Processed {line_count} lines.')
+        print(adjList)
+
+if __name__ == "__main__":
+    convert()
