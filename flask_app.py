742ef39dcab4247c591b2c2e140752de3d42fee3
Tom Moses <tom@tmoses.co.uk>
2022-01-01 04:25:09 +0000

    update to handle school and town navigation

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,26 @@
 from io import BytesIO
 from flask import Flask, request, render_template, send_file
 from scvToAdjList import convert 
-from mapper import getPath, getMap, getPlaces, getBlankMap
+from mapper import getSchoolPath, getMap, getSchoolPlaces, getBlankMap, getTownPath, getTownPlaces
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    places = getPlaces()
+    places = getTownPlaces()
     return render_template('index.html', places=places)
+
+@app.route('/town-mapper', methods=['POST'])
+def townMapper():
+    start = request.form['start']
+    end = request.form['end']
+    path = getTownPath(start,end)
+    return str(path)
+
+@app.route('/school')
+def school():
+    places = getSchoolPlaces()
+    return render_template('school.html', places=places)
 
 # https://stackoverflow.com/questions/8637153/how-to-return-images-in-flask-response
 @app.route('/blank-map')
@@ -16,17 +28,17 @@ def blankMap():
     filename = getBlankMap()
     return send_file(filename, mimetype='image/gif')
 
-@app.route('/mapper', methods=['POST'])
-def mapper():
+@app.route('/school-mapper', methods=['POST'])
+def schoolMapper():
     start = request.form['start']
     end = request.form['end']
-    path = getPath(start,end)
+    path = getSchoolPath(start,end)
     filename = getMap(path)
     return send_file(filename, mimetype='image/gif')
 
 @app.route('/sample-path')
 def samplePath():
-    places = getPlaces()
+    places = getSchoolPlaces()
     path = [(places[0],places[2]),(places[2],places[1])]
     filename = getMap(path)
     return send_file(filename, mimetype='image/gif')
@@ -45,7 +57,7 @@ def carShop():
 
 @app.route('/adj')
 def adj():
-    result = convert()
+    result = convert('school-graph.csv')
     return result
 
 if __name__ == '__main__':
--- mapper.py
+++ mapper.py
@@ -2,25 +2,36 @@ from PIL import Image, ImageDraw
 from mydijkstras import Dijkstra
 from scvToAdjList import convert 
 
-PLACES = ['Atrium','Biology Lab','Corridor','Dinner Hall']
-IMAGE_LOCATIONS = {'Atrium':(300,350),'Biology Lab':(1175,400),'Corridor':(475,700),'Dinner Hall':(1225,700)}
+SCHOOL_PLACES = ['Atrium','Biology Lab','Corridor','Dinner Hall']
+SCHOOL_IMAGE_LOCATIONS = {'Atrium':(300,350),'Biology Lab':(1175,400),'Corridor':(475,700),'Dinner Hall':(1225,700)}
 
-def getPlaces():
-    return PLACES
+TOWN_PLACES = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF']
 
-def getPath(start,end):
-    graph = convert()
-    dijkstras = Dijkstra(graph, "Biology Lab", "Dinner Hall")
+def getSchoolPlaces():
+    return SCHOOL_PLACES
+
+def getTownPlaces():
+    return TOWN_PLACES
+
+def getSchoolPath(start,end):
+    graph = convert('school-graph.csv')
+    dijkstras = Dijkstra(graph, start, end)
+    return dijkstras.getDestinationPath()
+
+def getTownPath(start,end):
+    graph = convert('town-graph.csv', symmetrical=True)
+    print(graph)
+    dijkstras = Dijkstra(graph, start, end)
     return dijkstras.getDestinationPath()
 
 # https://www.geeksforgeeks.org/python-pil-imagedraw-draw-line/
 # https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html
 def getMap(path):
     filename = "path-map.png"
-    with Image.open("map.png") as im:
+    with Image.open("school-map.png") as im:
         draw = ImageDraw.Draw(im)
         for pathPart in path:
-            startAndEndXYs = [IMAGE_LOCATIONS[pathPart[0]],IMAGE_LOCATIONS[pathPart[1]]]
+            startAndEndXYs = [SCHOOL_IMAGE_LOCATIONS[pathPart[0]],SCHOOL_IMAGE_LOCATIONS[pathPart[1]]]
             draw.line(startAndEndXYs, fill=(66, 99, 245,50), width=20)
             draw.line(startAndEndXYs, fill=(66, 135, 245,128), width=10)
         im.save(filename)
@@ -28,6 +39,6 @@ def getMap(path):
 
 def getBlankMap():
     filename = "blank-map.png"
-    with Image.open("map.png") as im:
+    with Image.open("school-map.png") as im:
         im.save(filename)
     return filename
