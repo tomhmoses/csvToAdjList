654fccef29b73d59ce0c7fb0e16c3d3153bdc8fc
Tom Moses <tom@tmoses.co.uk>
2022-01-01 04:22:33 +0000

    add method to return places, and update convert to handle symmetrical graphs

--- scvToAdjList.py
+++ scvToAdjList.py
@@ -1,11 +1,18 @@
 import csv
 
+def getPlaces(filename):
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            names = row
+            return names
+    
 
-def convert():
+def convert(filename, symmetrical = False):
     adjList = {}
     names = []
 
-    with open('sample1.csv') as csv_file:
+    with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
@@ -20,19 +27,20 @@ def convert():
             # if a line of distances
             else:
                 #print(row)
-                adjList[names[line_count]] = {}
+                startPlace = names[line_count]
+                if startPlace not in adjList:
+                    adjList[startPlace] = {}
                 for pos in range(len(row)):
-                    if pos != 0 and row[pos]:
-                        startPlace = names[line_count]
+                    if pos != 0 and row[pos] and int(row[pos]) > 0:
                         destinationPlace = names[pos]
                         distance = int(row[pos])
                         #print(f'Distance from {startPlace} to {destinationPlace} is {distance}')
-                        adjList[names[line_count]][destinationPlace] = distance
-
-
+                        adjList[startPlace][destinationPlace] = distance
+                        if symmetrical:
+                            if destinationPlace not in adjList:
+                                adjList[destinationPlace] = {}
+                            adjList[destinationPlace][startPlace] = distance
                 line_count += 1
-        #print(f'Processed {line_count} lines.')
-        #print(adjList)
         return adjList
 
 if __name__ == "__main__":
